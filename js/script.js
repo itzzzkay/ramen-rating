1ad8965fcf8badb61603a85daaa0216f722a6f6b
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-03-18 17:55:38 +0300

    basic design

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpeg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpeg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpeg", comment: "Delicious!" },
+    { id: 4, name: "Hiyashi Ramen", restaurant: "Ichiraku", image: "hiyashi.jpeg", comment: "Delicious!" },
+    { id: 5, name: "Kaisen Ramen", restaurant: "Umai", image: "kaisen.jpeg", comment: "Very nutritious!" },
+    { id: 6, name: "Shio Ramen", restaurant: "Yukihira", image: "shio.jpeg", comment: "Salt is balanced!" },
+    { id: 7, name: "Tantanmen Ramen", restaurant: "Nakiri", image: "tantanmen.jpeg", comment: "Delicious!" },
+    { id: 8, name: "Tsukumen Ramen", restaurant:"Ikumi", image: "tsukumen.jpeg", comment: "Take when cold!" }
  ];
+
+ function displayRamens() {
+
+ }
